c8e024e15710c8bf94186c93e4ef42892461eb73
Raunak Kedia <raunakkedia1@gmail.com>
2023-07-10 14:26:01 +0530

    ThemeProvider and Global Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import Container from "@/components/styles/Container.styled";
 import Header from "@/components/Header";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles, theme } from "./GlobalStyles";
 
 const App = () => {
 	return (
-		<>
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
 			<Header />
 			<Container>
 				<h1>Hello</h1>
 			</Container>
-		</>
+		</ThemeProvider>
 	);
 };
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { StyledHeader } from "./styles/Header.styled";
 
 const Header = () => {
 	return (
-		<StyledHeader bgColor="#ebfbff">
+		<StyledHeader>
 			<h1>Hubble</h1>
 		</StyledHeader>
 	);
